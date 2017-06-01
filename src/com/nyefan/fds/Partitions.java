a44bb974cff831ef8f948fd6d7377d52a046cbf3
Anthony St. Aubin <C-Anthony.StAubin@Charter.com>
2017-06-01 16:08:38 -0600

    HoarePartition

---
+++ src/com/nyefan/fds/Partitions.java
@@ -0,0 +1,35 @@
+package com.nyefan.fds;
+
+import java.util.Comparator;
+
+/**
+ * @author nyefan
+ */
+public class Partitions<T> {
+
+    public int hoarePartition(View<T> A, int p, Comparator<T> comparator) {
+        if (p < 0 || p >= A.length) { throw new ArrayIndexOutOfBoundsException(); }
+        if (A.length <= 0) { throw new IllegalArgumentException(); }
+
+        int a = 1;
+        int b = A.length - 1;
+
+        loop:
+        while (true) {
+            while (true) {
+                if (a > b) { break loop; }
+                if (comparator.compare(A.get(a), A.get(0)) >= 0) { break; }
+                a++;
+            }
+            while (comparator.compare(A.get(a), A.get(b)) < 0) {
+                b--;
+            }
+            if (a >= b) { break; }
+            A.swap(a, b);
+            a++;
+            b--;
+        }
+        A.swap(0, a - 1);
+        return a - 1;
+    }
+}
