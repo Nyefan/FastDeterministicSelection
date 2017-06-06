b95f36a8616a7c09fc5c035031d3dc065cdbe00d
Anthony St. Aubin <C-Anthony.StAubin@Charter.com>
2017-06-06 16:17:22 -0600

    Move quickselect; Properly interface Partition; Move Comparator into View

--- src/com/nyefan/fds/Partitions.java
+++ src/com/nyefan/fds/Partitions.java
@@ -1,21 +1,41 @@
 package com.nyefan.fds;
 
-import java.util.Comparator;
+import static com.nyefan.fds.RoteRoutines.median5;
 
 /**
  * @author nyefan
  */
 public class Partitions {
-    @FunctionalInterface
+
     public interface Partition<T> {
-        public int apply(View<T> A, Comparator<T> comparator);
+        public int apply(View<T> A, int p);
+    }
+
+    public static class BFPRTBaseline<T> implements Partition<T> {
+        @Override
+        public int apply(View<T> A, int p) {
+            if (A.length < 5) { return new Partitions.HoarePartition<T>().apply(A, A.length / 2); }
+            int i = 0;
+            int j = 0;
+
+            while (i + 4 < A.length) {
+                median5(A, i, i + 1, i + 2, i + 3, i + 4);
+                A.swap(i + 2, j);
+                i += 5;
+                j += 1;
+            }
+            quickSelect(new BFPRTBaseline<>(), A.range(0, j), j / 2);
+            return new HoarePartition<T>().apply(A, A.length / 2);
+        }
     }
 
     public static class HoarePartition<T> implements Partition<T> {
         @Override
-        public int apply(final View<T> A, final Comparator<T> comparator) {
+        public int apply(final View<T> A, int p) {
             if (A.length <= 0) { throw new IllegalArgumentException(); }
+            if (p < 0 || p >= A.length) { throw new IllegalArgumentException(); }
 
+            A.swap(0, p);
             int a = 1;
             int b = A.length - 1;
 
@@ -23,10 +43,10 @@ public class Partitions {
             while (true) {
                 while (true) {
                     if (a > b) { break loop; }
-                    if (comparator.compare(A.get(a), A.get(0)) >= 0) { break; }
+                    if (A.compare(a, 0) >= 0) { break; }
                     a++;
                 }
-                while (comparator.compare(A.get(a), A.get(b)) < 0) {
+                while (A.compare(0, b) < 0) {
                     b--;
                 }
                 if (a >= b) { break; }
@@ -36,6 +56,21 @@ public class Partitions {
             }
             A.swap(0, a - 1);
             return a - 1;
+        }
+    }
+
+    private static <T> void quickSelect(Partition<T> partition, View<T> A, int k) {
+        if (k >= A.length || k < 0) { return; }
+        while (true) {
+            int p = partition.apply(A, A.length / 2);
+            //RETURN
+            if (p == k) { return; }
+            if (p > k) {
+                A = A.range(0, p);
+            } else {
+                k = k - p - 1;
+                A = A.range(p + 1, A.length);
+            }
         }
     }
 }
--- src/com/nyefan/fds/View.java
+++ src/com/nyefan/fds/View.java
@@ -1,5 +1,7 @@
 package com.nyefan.fds;
 
+import java.util.Comparator;
+
 /**
  * @author nyefan
  */
@@ -8,18 +10,35 @@ public class View<T> {
     int i;
     int j;
 
-    public final int length;
+    public final Comparator<T> comparator;
+    public final int           length;
 
-    public View(T[] A, int i, int j) {
+    public View(T[] A, Comparator<T> comparator) {
+        this(A, 0, A.length, comparator);
+    }
+
+    public View(T[] A, int i, int j, Comparator<T> comparator) {
+        //This is intentional to allow the original array to continue being modified
+        //noinspection AssignmentToCollectionOrArrayFieldFromParameter
         this.A = A;
         this.i = i;
         this.j = j;
-        this.length = A.length;
+        this.length = j-i;
+        this.comparator = comparator;
+    }
+
+    public int compare(int a, int b) {
+        return comparator.compare(A[a], A[b]);
     }
 
     public T get(int k) {
-        if (k >= j - i || k < j - i) { throw new ArrayIndexOutOfBoundsException(); }
+        if (k >= j - i || k < 0) { throw new ArrayIndexOutOfBoundsException(); }
         return A[k + i];
+    }
+
+    public void sort(int k, int l) {
+        if (k >= j - i || k < 0 || l >= j - i || l < 0) { throw new ArrayIndexOutOfBoundsException(); }
+        if (comparator.compare(A[k], A[l]) > 0) { swap(k, l); }
     }
 
     public void swap(int k, int l) {
@@ -30,9 +49,7 @@ public class View<T> {
     }
 
     public View<T> range(int k, int l) {
-        if (k >= j - i || k < 0 || l >= j - i || l < 0 || l < k) { throw new ArrayIndexOutOfBoundsException(); }
-        this.i += k;
-        this.j = i + l;
-        return this;
+        if (k >= j - i || k < 0 || l > j - i || l < 0 || l < k) { throw new ArrayIndexOutOfBoundsException(); }
+        return new View<>(A, i + k, i + l, comparator);
     }
 }
